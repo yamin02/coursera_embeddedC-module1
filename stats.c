c03f8454713259c877c8f7a1c4d49044c68ed257
Yamin02 <yaminulhoque@ug.me.buet.ac.bd>
2020-05-27 21:35:24 +0600

    The final project

--- stats.c
+++ stats.c
@@ -27,20 +27,20 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
+int main() {
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
+    unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                                 114, 88,   45,  76, 123,  87,  25,  23,
+                                 200, 122, 150, 90,   92,  87, 177, 244,
+                                 201,   6,  12,  60,   8,   2,   5,  67,
+                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
-  sort_array(test,SIZE);
-  print_array(test,SIZE);
-  print_statistics(test,SIZE);
 
+    sort_array(test,SIZE);
+    print_array(test,SIZE);
+    printf("\n");
+    print_statistics(test, SIZE);
+    return 0;
 }
 
-/* Add other Implementation File Code Here */
+
--- stats.h
+++ stats.h
@@ -2,10 +2,10 @@
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
@@ -21,52 +21,78 @@
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+#include <stdio.h>
+
+unsigned int roundof(double num){
+    int intpart = (int)num;
+    double decpart = num - intpart;
+    unsigned int ans = (decpart>= 0.5)? intpart+1 : intpart;
+    return ans;
+}
+/* Add Your Declarations and Function Comments here */
 void print_array(unsigned char arr[], unsigned int size ){
-    //function body
+    for (int i = 0; i<size ; i++)
+    {printf("%d ", arr[i]);
+        if ((i+1)%8 == 0 && i>0)
+            printf("\n");}
 }
-void swap(unsigned int *a1 , unsigned int *a2){
-    //function body
+void swap(unsigned char *a1 , unsigned char *a2){
+    int temp = *a1;
+    *a1 = *a2;
+    *a2 = temp;
 }
-unsigned find_median(unsigned char arr[], unsigned int size){
-    unsigned int median;
-    //function body
-    return median;
+unsigned int find_median(unsigned char arr[], unsigned int size){
+    double median;
+    if (size % 2 == 1){
+        int num = (int)(size + 1)/2;
+        median = arr[num];}
+    else
+    {int x1 = (int)size/2;
+        median = (double)(arr[x1]+arr[x1+1])/2;}
+    return roundof(median);
 }
-unsigned int find_mean(unsigned char arr[], unsigned int size){
-    unsigned int mean;
-    //function body
-    return mean;
+
+unsigned int  find_mean(unsigned char arr[], unsigned int size){
+    double mean;
+    double sum = 0;
+    for (int i = 0 ; i<size ; i++){
+        sum+=arr[i];
+    }
+    mean = sum/size;
+    return roundof(mean);
+
 }
 unsigned int find_maximum(unsigned char arr[], unsigned int size){
-    unsigned int max;
-    //function body
+    unsigned int max=0;
+    for (int i= 0 ; i<size; i++){
+        if(arr[i]>max)
+            max = arr[i];
+    }
     return max;
 }
 unsigned int find_minimum(unsigned char arr[], unsigned int size){
-    unsigned int min;
-    // function body
-    return max;
+    unsigned int min=arr[0];
+    for (int i= 0 ; i<size; i++){
+        if(arr[i]<min)
+            min = arr[i];}
+    return min;
 }
 void sort_array(unsigned char arr[], unsigned int size){
-    //function body
-}
-void print_statistics(unsigned char arr[], unsigned int size){
-    //function body
+    for (int i = 0; i<size-1; i++){
+        for(int a = 0; a<(size-1-i); a++)
+        { if (arr[a]<arr[a+1])
+                swap(&arr[a+0],&arr[a+1]);}
+    }
 }
 
-/**
- * @brief <Add Brief Description of Function Here>
- *
- * <Add Extended Description Here>
- *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
- */
+void print_statistics(unsigned char arr[], unsigned int size){
+    printf("Mean of the given data = %d\n", find_mean(arr,size));
+    printf("Median of the given data = %d\n", find_median(arr,size));
+    printf("Maximum value of the given data = %d\n", find_maximum(arr,size));
+    printf("Minimum of the given data = %d", find_minimum(arr,size));
+
+}
+
 
 
 #endif /* __STATS_H__ */
